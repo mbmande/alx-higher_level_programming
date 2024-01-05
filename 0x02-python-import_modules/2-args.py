24b04b8a8b3c27b3b9b5b27de923487b946103b8
mbmande <mbsmande@gmail.com>
2024-01-04 17:10:10 -0800

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    why = len(argv) - 1
+    if why < 1:
+        print("{} value: ".format(why))
+    elif why == 1:
+        print("{} value: ".format(why))
+    else:
+        print("{} value: ".format(why))
+
+    for o in range(why):
+        print("{}: {:s}".format(o + 1, argv[o + 1]))
