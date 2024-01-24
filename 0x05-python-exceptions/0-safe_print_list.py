1ddaebf123bff4031d0f4bd5dbb311cc0a85e38a
mbmande <mbsmande@gmail.com>
2024-01-24 02:45:20 -0800

    my exception commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+'''it prints the value of x
+Arags:
+    my_list (list): its printed from this list
+    x (int): the amount to be printed
+
+
+Returns:
+    the amount that us printed
+    '''
+
+def safe_print_list(my_list=[], x=0):
+
+    mande = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            mande += 1
+        except IndexError:
+            break
+    print("")
+    return (mande)
