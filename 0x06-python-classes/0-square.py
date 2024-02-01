2ea61732c951227842f920c03549b79d0c0a2ac1
mbmande <mbsmande@gmail.com>
2024-02-01 06:09:12 -0800

    class.square commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""the module"""
+
+
+class Squre:
+    """it defines a class called square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""the modules"""
+
+class Square:
+    """square class"""
+    def __init__(self, size):
+        self.__size = size
+
