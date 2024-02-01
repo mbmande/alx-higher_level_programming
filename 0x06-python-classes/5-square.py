81f2c3e40e93c73a9a5f61446e60044e0cb9b526
mbmande <mbsmande@gmail.com>
2024-02-01 08:38:28 -0800

    my square5 commit

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -23,7 +23,7 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
- 
+
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
