ccc1ccd8c34f8847f044dc03dc4eb0cf03921bc2
mbmande <mbsmande@gmail.com>
2024-02-08 16:44:20 -0800

    raising exceptions

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""the modules"""
+
+
+class BaseGeometry:
+    """the class"""
+    def area(self):
+        
+        """it raises an exception"""
+        raise Exception("area() is not implemented")
