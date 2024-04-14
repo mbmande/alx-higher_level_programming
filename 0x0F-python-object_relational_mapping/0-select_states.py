6e446be581ca1051926e4f3543479d959437104d
mbmande <mbsmande@gmail.com>
2024-04-14 16:56:41 -0700

    selection of state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,9 @@ import sys
 import MySQLdb
 
 def list_states (username, password, database):
-
+    """============
+    =============
+    """
     db = MySQLdb.connect(host='localhost',\
             port=3306,\
             user=username,\
