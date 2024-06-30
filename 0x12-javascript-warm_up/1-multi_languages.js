7f3a629f8dd93e6d65ad361d2a560205845caf58
mbmande <mbsmande@gmail.com>
2024-06-30 01:39:48 -0700

    coding is fun

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+for (let I = 0; I < 3; I++) {
+	if (I == 0) {
+		console.log("C is fun");
+	}
+	else if (I == 1){
+		console.log("Python is cool");
+	}
+	else{
+		console.log("JavaScript is amazing");
+	}
+}
