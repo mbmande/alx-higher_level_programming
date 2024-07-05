4757ec47de5e78095a125d57446078f8fcee32f1
mbmande <mbsmande@gmail.com>
2024-07-05 02:21:11 +0000

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,17 +4,17 @@ sgdsjdsjdsdkjldksdjk
 """
 
 def find_peak(list_of_integers):
-        size = len(list_of_integers)
-            mid = size // 2
+    size = len(list_of_integers)
+    mid = size // 2
 
-                if size == 0:
-                            return None
+    if size == 0:
+        return None
 
-                            while True:
-                                        mid_e = size // 2
-                                                if mid < size - 1 and list_of_integers[mid] < list_of_integers[mid + 1]:
-                                                                mid = mid + mid_e // 2
-                                                                        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-                                                                                        mid = mid - mid_e // 2
-                                                                                                else:
-                                                                                                                return list_of_integers[mid]
+    while True:
+        mid_e = size // 2
+        if mid < size - 1 and list_of_integers[mid] < list_of_integers[mid + 1]:
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            mid = mid - mid_e // 2
+        else:
+            return list_of_integers[mid]
