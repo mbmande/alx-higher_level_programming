9ab764ce1a5289cb7bb12327f8ee3f83ffd45b41
mbmande <mbsmande@gmail.com>
2024-07-30 17:00:29 -0700

    star count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,31 +1,22 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const request = require('request');
 const url = process.argv[2];
 const characterId = '18';
 let count = 0;
 
-const handleError = (error) => {
-  console.log(error);
-};
-
-const processData = (body) => {
-  const data = JSON.parse(body);
-  data.results.forEach((film) => {
-    film.characters.forEach((character) => {
-      if (character.includes(characterId)) {
-        count += 1;
-      }
-    });
-  });
-  console.log(count);
-};
-
 request.get(url, (error, response, body) => {
   if (error) {
-    handleError(error);
+    console.log(error);
   } else {
-    processData(body);
+    const data = JSON.parse(body);
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          count += 1;
+        }
+      });
+    });
+    console.log(count);
   }
 });
-
